6ce08c1296f32eb531a85fe116b96d5b38d9231d
pedesen <pedesen@googlemail.com>
2013-10-13 01:10:29 +0200

    corrected typo

--- fagungis/tasks.py
+++ fagungis/tasks.py
@@ -267,7 +267,7 @@ def _verify_sudo():
 
 def _install_nginx():
     # add nginx stable ppa
-    sudo("add-apt -y install nginx")
+    sudo("apt-get -y install nginx")
     sudo("/etc/init.d/nginx start")
 
 
